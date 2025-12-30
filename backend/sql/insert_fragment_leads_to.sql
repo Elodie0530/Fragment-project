c1dff09e4be8dc2774258139585618ced03bb758
Elodie0530 <uneondine@gmail.com>
2025-12-30 14:47:37 +0100

    Styling css and sql for readability in development

--- backend/sql/insert_fragment_leads_to.sql
+++ backend/sql/insert_fragment_leads_to.sql
@@ -56,40 +56,40 @@ INSERT INTO lead_to (action, start_id, end_id, position) VALUES
 -- Chapitre 1 — Carte départ
 (
     "La place de la fontaine",
-    (select id from chapter where history_id = 1 and number=1),
-    (select id from chapter where history_id = 1 and number=2), 1
+    (select id from chapter where history_id = 1 and number = 1),
+    (select id from chapter where history_id = 1 and number = 2), 1
 ),
 
 -- Chapitre 2 — Place de la fontaine
 (
     "Aller à l’auberge Duergar",
-    (select id from chapter where history_id = 1 and number=2),
-    (select id from chapter where history_id = 1 and number=11), 1
+    (select id from chapter where history_id = 1 and number = 2),
+    (select id from chapter where history_id = 1 and number = 11), 1
 ),
 (
     "Visiter l’école du village",
-    (select id from chapter where history_id = 1 and number=2),
-    (select id from chapter where history_id = 1 and number=22), 2
+    (select id from chapter where history_id = 1 and number = 2),
+    (select id from chapter where history_id = 1 and number = 22), 2
 ),
 (
     "Explorer la bijouterie",
-    (select id from chapter where history_id = 1 and number=2),
-    (select id from chapter where history_id = 1 and number=33), 3
+    (select id from chapter where history_id = 1 and number = 2),
+    (select id from chapter where history_id = 1 and number = 33), 3
 ),
 
 -- Chapitre 11 — L’Auberge Duergar
 (
     "Entrer dans l’auberge",
-    (select id from chapter where history_id = 1 and number=11),
-    (select id from chapter where history_id = 1 and number=34), 1
+    (select id from chapter where history_id = 1 and number = 11),
+    (select id from chapter where history_id = 1 and number = 34), 1
 ),
 (
     "Revenir place de la fontaine",
-    (select id from chapter where history_id = 1 and number=11),
-    (select id from chapter where history_id = 1 and number=2), 2
+    (select id from chapter where history_id = 1 and number = 11),
+    (select id from chapter where history_id = 1 and number = 2), 2
 ),
 (
     "Se rendre à la bourse aux minéraux",
-    (select id from chapter where history_id = 1 and number=11),
-    (select id from chapter where history_id = 1 and number=6), 3
+    (select id from chapter where history_id = 1 and number = 11),
+    (select id from chapter where history_id = 1 and number = 6), 3
 );
